42dc77ded1aab1d7eec61388c0e00412d541ae41
PhanTruong257 <phantruong0725@gmail.com>
2025-03-26 08:24:47 +0700

    Create Project

---
+++ src/test/java/com/example/reservationhotel/ReservationHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
